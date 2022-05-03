ddd700bf2b45e1e8683524b7f3d7b02dd19bedc8
xyzza <yaxyzza@gmail.com>
2022-05-03 11:46:07 +0300

    fix

--- anagram.py
+++ anagram.py
@@ -1,20 +1,20 @@
 from typing import List
 
 DATA = [
-    (["code", "anagram", "doce", "marganana", "wut"], ["anagram", "code", "wut"]),
-    (["idea", "god", "daidee", "marganana", "odog"], ["god", "idea", "marganana"]),
+    (["code", "anagram", "doce", "marganana", "wut"], ["anagram", "code"]),
+    (["idea", "god", "daidee", "marganana", "odog"], ["god", "idea"]),
 ]
 
 
 def fun_anagram(words: List[str]) -> List[str]:
-    _words = set()
-    result = []
+    result = {}
     for word in words:
-        _key = frozenset(word)
-        if _key not in _words:
-            _words.add(_key)
-            result.append(word)
-    return sorted(result)
+        key = frozenset(word)
+        if key in result:
+            result[key] = (result[key][0], result[key][1] + 1)
+        else:
+            result[key] = (word, 1)
+    return sorted([x[0] for x in result.values() if x[1] > 1])
 
 
 def main():
