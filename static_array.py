8a2e1ee8cac41d58295cb685004f43d5693bb14a
Timur Akhmadiev <yaxyzza@gmail.com>
2023-05-07 13:10:03 +0300

    commit static array

---
+++ static_array.py
@@ -0,0 +1,13 @@
+class StaticArray:
+
+    def __init__(self, sequence):
+        self._data = [el for el in sequence]
+
+    def __iter__(self):
+        return self._data
+
+    def get_at(self, i):
+        return self._data[i]
+
+    def set_at(self, i, value):
+        self._data[i] = value
